aff7891709ef077511fe4d2f7cd94aee5e0ba7dd
Brandon Liu <bliu1002001@gmail.com>
2024-01-25 21:59:41 -0800

    Finished PA1

--- ocean/src/clans.rs
+++ ocean/src/clans.rs
@@ -1,39 +1,72 @@
+use crate::crab::Crab;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct ClanSystem {
     // TODO: add necessary fields
+    clans: HashMap<String, Vec<String>>,
 }
 
 impl ClanSystem {
     pub fn new() -> ClanSystem {
-        unimplemented!();
+        ClanSystem {
+            clans: HashMap::new(),
+        }
+    }
+
+    pub fn add_clan_member(&mut self, clan_id: &str, crab_name: &str) {
+        let entry = self.clans.entry(clan_id.to_string()).or_insert(vec![]);
+        entry.push(crab_name.to_string());
     }
 
     /**
      * Returns a list of the names of the clan members for the given clan id.
      */
     pub fn get_clan_member_names(&self, clan_id: &str) -> Vec<String> {
-        unimplemented!();
+        match self.clans.get(clan_id) {
+            Some(vec) => {
+                return vec.clone()
+            }
+            _ => {
+                return Vec::new()
+            }
+        }
     }
+
 
     /**
      * Returns the number of clans currently in existence.
      */
     pub fn get_clan_count(&self) -> usize {
-        unimplemented!();
+        self.clans.len()
     }
 
     /**
      * Returns the number of clan members for the given clan id.
      */
     pub fn get_clan_member_count(&self, clan_id: &str) -> usize {
-        unimplemented!();
+        match self.clans.get(clan_id) {
+            Some(vec) => {
+                return vec.len()
+            }
+            None => {
+                return 0
+            }
+        }
     }
 
     /**
      * Returns the id of the clan with the most number of members, or None if such a clan does not exist.
      */
     pub fn get_largest_clan_id(&self) -> Option<String> {
-        unimplemented!();
+        let mut largest = 0;
+        let mut largest_clan_id = None;
+        for (id, vec) in self.clans.iter(){
+            if vec.len() > largest {
+                largest = vec.len();
+                largest_clan_id = Some(id.clone());
+            }
+        }
+        largest_clan_id
     }
 }
